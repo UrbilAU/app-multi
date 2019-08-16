8b57371cc20622e787cec60b5f845fad146c44a8
Urbil <2garribillagaurbil@gmail.com>
2019-08-16 13:08:29 +0200

    Octavo commit. La orden de las opciones del commit cambiada

--- src/example.js
+++ src/example.js
@@ -1,3 +1,4 @@
 console.log('esto es un archivo de ejemplo');
 console.log('añadiendo línea para ver más opciones en el commit');
 console.log('esta vez probaremos a hacer commit saltandonos el add');
+console.log('esta vez probaremos a hacer commit cambiando las opciones de orden');
