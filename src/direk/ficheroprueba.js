5cb0f2c15be9da624b00b35306a2291487ecb38f
Urbil <uarribillaga001@ikasle.ehu.eus>
2019-08-31 10:51:46 +0200

    git mergetool irekiko dugu

--- src/direk/ficheroprueba.js
+++ src/direk/ficheroprueba.js
@@ -3,3 +3,4 @@ console.log('segundo tag simple');
 console.log('ahora un tag anotado');
 console.log('adarra branch');
 console.log('beste adarra branch bat');
+console.log('git mergetool irekiko dugu');
