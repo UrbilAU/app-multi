2f340c0ab90409732c8d1532c341b19acea362b1
Urbil <uarribillaga001@ikasle.ehu.eus>
2019-10-06 22:52:50 +0200

    commit before i18n

---
+++ src/messages.js
@@ -0,0 +1,9 @@
+import { defineMessages } from 'react-intl'
+
+const messages = defineMessages({
+    greeting: {
+        id: 'src.greeting',
+        description: 'Hello world message',
+        defaultMessage: 'Hello world!'
+    }
+})
