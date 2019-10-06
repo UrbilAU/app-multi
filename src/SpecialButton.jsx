30883e7c9ff8a06af962bb4eff15c32db96bac31
Urbil <uarribillaga001@ikasle.ehu.eus>
2019-10-06 12:23:29 +0200

    amend includes SpecialButton

--- src/SpecialButton.jsx
+++ src/SpecialButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 class SpecialButton extends React.Component {
     render(){
-        return <button onClick={this.props.handleClick}>ALDATU LENGOAIA</button>
+        return <button onClick={this.props.handleClick}>ALDATU HIZKUNTZA</button>
     }
 }
 
